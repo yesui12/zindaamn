e142f798a95c41fa9e878a89c406daa310c92a29
Yesui <yesui1209@gmail.com>
2021-05-06 15:31:33 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        dark: "#1A1C3C",
+      },
+      fontSize: {
+        "2xs": "0.65rem",
+        "ssm": "0.80rem",
+      },
+      borderWidth: {
+        "1": "1px",
+        "1.5": "1.5px",
+      },
+      height: {
+        "18": "4.5rem",
+        "19": "4.8rem",
+        "22": "5.5rem",
+        "30": "7.5rem",
+        "50": "12.7rem",
+        "82": "22rem",
+        "100": "28rem",
+      },
+      inset: {
+        "22": "5.5rem",
+        "18": "4.5rem",
+      },
+      padding: {
+        "58": "14.5rem",
+      },
+      fontFamily: {
+        main: "Open Sans Condensed",
+        notoserif: "Noto Serif",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
